726aea4eb52974560c94813f114249dd71c70dd8
Alex Dorrell <alexdorrell9@gmail.com>
2022-01-12 14:13:31 -0500

    more comments

--- pkg/curses/console.go
+++ pkg/curses/console.go
@@ -10,6 +10,8 @@ type console struct {
 	buff   *ringbuff.RingBuff
 }
 
+//size is used for the ring buffer
+//should be height of window
 func newConsole(size int) *console {
 	b := ringbuff.NewRingBuff(size)
 	return &console{
@@ -18,6 +20,7 @@ func newConsole(size int) *console {
 	}
 }
 
+//initialize console window
 func (con *console) init(h, w, r, c int) {
 	window, err := gc.NewWindow(h, w, r, c)
 	if err != nil {
@@ -27,16 +30,17 @@ func (con *console) init(h, w, r, c int) {
 	con.window.SetBackground(gc.ColorPair(COLOR_CONSOLE))
 }
 
+//adds message to console
 func (con *console) AddMessage(s string) {
 	win := con.window
 	buf := con.buff
 
-	//todo split on length?
+	//todo split on length >= width?
 	buf.Add(s)
 
+	//easier to clear the console once
 	win.Erase()
 	for i := 0; i < buf.GetSize(); i++ {
-		//fix
 		line, _ := buf.Get(i)
 		win.ColorOn(COLOR_CONSOLE)
 		win.MovePrint(i, 0, line)
